7c6a6be523b3508f63f205a40cbc1c938e66ff7a
Nathan Hood <nathanhood@me.com>
2014-07-30 11:06:33 -0500

    Integration tests for training paths

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,12 +1,23 @@
 # Changelog
 
+## July 30, commit 4
+
+  * Integration specs for the training paths menu, which compelled us to write
+  * Integration specs for creating a trining path
+
+Note:
+
+## July 30, commit 3
+
+  * Commit of shame. We forgot to smoke test
+
 ## July 30, commit 2
 
   * `touch zss`
   * `chmod +x zss`
   * Add Shebang to zss file (#!/usr/bin/env ruby)
     * This is for bash to know how to run file/ tells shell that zss is ruby file
-  
+  * Prints Zombie
 
 ## July 30, commit 1
 
--- spec/main_menu_integration_spec.rb
+++ spec/main_menu_integration_spec.rb
@@ -30,16 +30,21 @@ NUGGGGGH MUST EAT BRAINS
       expect(actual).to include(expected)
     end
 
-    it "should print list of training paths"
+    it "should print list of training paths" do
+      pending "The ability to add training paths via the CLI"
+      TrainingPath.create(name: "Running")
+      TrainingPath.create(name: "Hand-to-Hand Combat")
+      TrainingPath.create(name: "Sneaking")
+      actual = `./zss`
       expected = "1. Running" +
                  "2. Hand-to-Hand Combat" +
                  "3. Sneaking"
       expect(actual).to include(expected)
     end
 
-    # it "should print graphs that match the progress down the training paths" do
-    #   pending "implementation of skills"
-    #   fail
-    # end
+    it "should print graphs that match the progress down the training paths" do
+      pending "implementation of skills"
+      fail
+    end
   end
 end
