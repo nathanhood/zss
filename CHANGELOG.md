8b84cf096f8350e6802e5dcb8dcab51996af65f8
Nathan Hood <nathanhood@me.com>
2014-07-30 11:57:56 -0500

    brought in eliza 'adding_training_paths_integration' and 'training_path_spec' files

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,11 +1,19 @@
 # Changelog
 
+## July 30, commit 5
+
+  * `mkdir -p app/models` to create the models dir.
+  * Created `spec/models/training_path_spec.rb`
+  * Moved most validation related specs into the TrainingPath unit specs
+  * Created the barest skeleton of the TrainingPath class
+  * Added a line to `spec_helper.rb` to require all classes in the `app` folder
+
 ## July 30, commit 4
 
   * Integration specs for the training paths menu, which compelled us to write
   * Integration specs for creating a trining path
 
-Note: 
+Note: The training paths are all expected to fail in this commit.
 
 ## July 30, commit 3
 
--- spec/integration/adding_training_paths_integration_spec.rb
+++ spec/integration/adding_training_paths_integration_spec.rb
@@ -1,79 +1,26 @@
 RSpec.describe "Adding training paths" do
   context "valid input" do
-    before do
-      run_zss_with_input("add", "Sneaking")
-    end
+    let(:output){ run_zss_with_input("add", "Sneaking") }
 
     it "should add a record" do
       expect(TrainingPath.count).to == 1
     end
-
-    it "should save the record" do
+    it "should save the record accurately" do
       expect(TrainingPath.last.name).to == "Sneaking"
     end
-
     it "should print a success message" do
-      expect(output).to include("Sneaking has been added to the list of
-      training paths")
+      expect(output).to include("Sneaking has been added to the list of training paths")
     end
   end
-
   context "invalid input" do
-    context "really long strings" do
-      before do
-        long_string = "foo" * 12
-        run_zss_with_input("add", long_string)
-      end
+    let(:long_string){ "foo" * 12 }
+    let(:output){ run_zss_with_input("add", long_string) }
 
-      it "should print an appropriate error message" do
-        expect(output).to include("Name must be less than 30 characters")
-      end
-
-      it "shouldn't save a new record" do
-        expect(TrainingPath.count).to == 0
-      end
+    it "prints the error message for the type of invalid input" do
+      expect(output).to include("name must be less than 30 characters")
     end
-
-    context "empty input" do
-      before do
-        run_zss_with_input("add", "12")
-      end
-
-      it "should print an appropriate error message" do
-        expect(output).to include("Name cannot be blank")
-      end
-
-      it "shouldn't save a new record" do
-        expect(TrainingPath.count).to == 0
-      end
-    end
-
-    context "numbers" do
-      before do
-        run_zss_with_input("add", "12")
-      end
-
-      it "should print an appropriate error message" do
-        expect(output).to include("Name must must include letters")
-      end
-
-      it "shouldn't save a new record" do
-        expect(TrainingPath.count).to == 0
-      end
-    end
-
-    context "a duplicate path name" do
-      before do
-        run_zss_with_input("add", "Running")
-      end
-
-      it "should print an appropriate error message" do
-        expect(output).to include("A path with that name already exists")
-      end
-
-      it "shouldn't save a new record" do
-        expect(TrainingPath.count).to == 0
-      end
+    it "shouldn't save the new record" do
+      expect(TrainingPath.count).to == 0
     end
   end
 end
